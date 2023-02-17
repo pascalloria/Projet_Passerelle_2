f07a11443060cfde6247c34d72da99c8d1147ad4
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-02-17 19:37:11 +0100

    ajout des 1eres vues articles

--- public/index.php
+++ public/index.php
@@ -2,27 +2,32 @@
 
 require_once("../controller/controller.php");
 
-    try{
-         if (!empty($_GET["page"])) {
+try {
+    if (!empty($_GET["page"])) {
 
-        if ($_GET["page"]==="home"){
+        if ($_GET["page"] === "home") {
             home();
+        } else if ($_GET["page"] === "createProject") {
+            createProject();
+        } else if ($_GET['page'] === 'new-article') {
             
-        } else if ($_GET["page"]==="createProject"){
-           createProject();
+            if (!empty($_POST['title']) && !empty($_POST['article'])) {
+                $title_article = htmlspecialchars($_POST['title']);
+                $article = htmlspecialchars($_POST['article']);
+                $id_user = 1; // mise en place des sessions plus tard
+                addArticle($title_article, $article, 1);
+            } else {
+                newArticleForm();
+            }
+        } else if ($_GET['page'] === 'articles') {
+            articles();
         } else {
             throw new Exception("Cette page n'existe pas");
         }
-    } else{
-         home();
-    } 
-    
-    } catch( Exception $e){
-        $error = $e->getMessage();
-        require("../view/errorView.php");
+    } else {
+        home();
     }
-
-   
-   
-
-
+} catch (Exception $e) {
+    $error = $e->getMessage();
+    require("../view/errorView.php");
+}

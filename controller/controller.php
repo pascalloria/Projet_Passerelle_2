3b837e4c378bcc3dbc587b891de4b7491b3a8cde
pascalloria <dev.pascalloria@gmail.com>
2023-02-17 21:59:11 +0100

    controller edit

--- controller/controller.php
+++ controller/controller.php
@@ -58,17 +58,5 @@
     }
    
     
-    function addProject($title,$content,$id_user) {
-        // Model
-        $projet = new ProjectManager;
-        $result = $projet->addProject($title,$content,$id_user);
-        echo $result;
-        if ($result){
-            header("location:index.php?page=home&&succes=1&&message=L'article à bien été créér");
-        } else {
-            throw new Exception("L'article n'a pas pu etre créer");
-        }
-
-
-    }
+  
 
